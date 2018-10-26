a14785a746d28576b5337a05caf01074f2b8c3d7
Michael <mike8383@gmail.com>
2018-10-26 22:49:27 +0300

    Updates

---
+++ precache-manifest.6896cbc901db3ceb77540fc5e560bb7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Herolo-Cinema/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "91ed450c52fbe1f60cf5",
+    "url": "/Herolo-Cinema/static/js/runtime~main.91ed450c.js"
+  },
+  {
+    "revision": "5295d299dc1f87666e36",
+    "url": "/Herolo-Cinema/static/js/main.5295d299.chunk.js"
+  },
+  {
+    "revision": "56d2df506b34722a1ab7",
+    "url": "/Herolo-Cinema/static/js/1.56d2df50.chunk.js"
+  },
+  {
+    "revision": "5295d299dc1f87666e36",
+    "url": "/Herolo-Cinema/static/css/main.02357bc9.chunk.css"
+  },
+  {
+    "revision": "56d2df506b34722a1ab7",
+    "url": "/Herolo-Cinema/static/css/1.f34439dc.chunk.css"
+  },
+  {
+    "revision": "1405595bf49d08ff8f5cb25e1aa44f9b",
+    "url": "/Herolo-Cinema/index.html"
+  }
+];
