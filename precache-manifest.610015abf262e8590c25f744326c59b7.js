54df51b10f4bf6b192f45ae30c8057c6c3922c08
Michael <mike8383@gmail.com>
2018-10-27 18:46:18 +0300

    Updates

--- precache-manifest.610015abf262e8590c25f744326c59b7.js
+++ precache-manifest.610015abf262e8590c25f744326c59b7.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/Herolo-Cinema/static/js/runtime~main.91ed450c.js"
   },
   {
-    "revision": "1f14ecd1cd2796cfebad",
-    "url": "/Herolo-Cinema/static/js/main.1f14ecd1.chunk.js"
+    "revision": "dc34d0d01902515cf096",
+    "url": "/Herolo-Cinema/static/js/main.dc34d0d0.chunk.js"
   },
   {
-    "revision": "4b7d059b9ded2bf4369a",
-    "url": "/Herolo-Cinema/static/js/1.4b7d059b.chunk.js"
+    "revision": "4ba15f14a6ed3277c4a7",
+    "url": "/Herolo-Cinema/static/js/1.4ba15f14.chunk.js"
   },
   {
-    "revision": "1f14ecd1cd2796cfebad",
-    "url": "/Herolo-Cinema/static/css/main.18f3dd40.chunk.css"
+    "revision": "dc34d0d01902515cf096",
+    "url": "/Herolo-Cinema/static/css/main.6c2b32af.chunk.css"
   },
   {
-    "revision": "4b7d059b9ded2bf4369a",
+    "revision": "4ba15f14a6ed3277c4a7",
     "url": "/Herolo-Cinema/static/css/1.f34439dc.chunk.css"
   },
   {
-    "revision": "67abaab3e4498abd2ded674f799684c6",
+    "revision": "4cbe508cfc60897ee811c37e6d7bfdaf",
     "url": "/Herolo-Cinema/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Herolo-Cinema/precache-manifest.834ad241c6e3d8ae7338364836817f24.js"
+  "/Herolo-Cinema/precache-manifest.610015abf262e8590c25f744326c59b7.js"
 );
 
 workbox.clientsClaim();
